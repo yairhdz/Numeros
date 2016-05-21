7a14cacec300666655e528ba18d4b07d64bf00ea
yairhdz <yairhdz0416@gmail.com>
2016-05-20 22:44:40 -0500

    Nuevo diseño del menu con imagenes

--- view/menu.lua
+++ view/menu.lua
@@ -16,12 +16,12 @@ local scene = composer.newScene( sceneName )
 
 local function buttonListener( event )
   print("TOUCHED".. event.target:getLabel())
-  local touchedButton = event.target:getLabel()
-  if ( touchedButton == "Aprende" ) then
+  local touchedButton = event.target.id
+  if ( touchedButton == "aprende" ) then
     composer.gotoScene("view.secuencia", { effect = "slideRight" } )
-  elseif ( touchedButton == "Ordena" ) then
+  elseif ( touchedButton == "ordena" ) then
     composer.gotoScene("view.ordenar", { effect = "slideRight" } )
-  elseif ( touchedButton == "Cuenta" ) then
+  elseif ( touchedButton == "cuenta" ) then
     composer.gotoScene("view.contar", { effect = "slideRight" } )
   end
 end
@@ -34,39 +34,46 @@ function scene:create( event )
   -- e.g. add display objects to 'sceneGroup', add touch listeners, etc
   local centerX = display.contentCenterX
   local centerY = display.contentCenterY
-  local fondo = display.newRect(centerX, centerY, display.actualContentWidth, display.contentHeight)
+--  local fondo = display.newRect(centerX, centerY, display.actualContentWidth, display.contentHeight)
+  local fondo = display.newImageRect("assets/menu.png", display.contentWidth, display.contentHeight)
+  fondo.x = centerX
+  fondo.y = centerY
   local secuenciaButton = widget.newButton(
     {
-      label = "Aprende",
---      defaultFile = "assets/play_button.png",
---      overFile = "assets/play_button_hover.png",
-      x = centerX,
-      y = centerY - 50,
+      defaultFile = "assets/controls/aprende_button.png",
+      width = 370, 
+      height = 66,
+      x = centerX + 160,
+      y = centerY - 10,
       onEvent = buttonListener
     }
   )
+  secuenciaButton.id = "aprende"
 
   local ordenarButton = widget.newButton(
     {
-      label = "Ordena",
---      defaultFile = "assets/play_button.png",
+      defaultFile = "assets/controls/ordenar_button.png",
 --      overFile = "assets/play_button_hover.png",
-      x = centerX, 
-      y = centerY,
+      width = 370, 
+      height = 66,
+      x = centerX + 160,
+      y = centerY + 65,
       onEvent = buttonListener
     }
   )
+  ordenarButton.id = "ordena"
 
   local contarButton = widget.newButton(
     {
-      label = "Cuenta",
---      defaultFile = "assets/play_button.png",
---      overFile = "assets/play_button_hover.png",
-      x = centerX, 
-      y = centerY + 50,
+      defaultFile = "assets/controls/contar_button.png",
+      width = 370, 
+      height = 66,
+      x = centerX + 160,
+      y = centerY + 145,
       onEvent = buttonListener
     }
   )
+  contarButton.id = "cuenta"
 
   sceneGroup:insert(fondo)
   sceneGroup:insert(secuenciaButton)
